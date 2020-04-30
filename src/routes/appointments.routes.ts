fcd857e20c6511f8242296edc6d2713c75644814
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-04-30 16:29:14 -0300

    :art: create list method in repository to appointment

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -6,6 +6,12 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (requeset, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
   const parsedDate = startOfHour(parseISO(date));

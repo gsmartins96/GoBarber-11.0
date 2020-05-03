3505feebd4140117e482d447299c75e9382ffe22
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-05-03 13:20:24 -0300

    :construction: configure models and migrations
    change provider to provider_id and make a relation with both

--- src/database/migrations/1588360667115-CreateAppointments.ts
+++ src/database/migrations/1588360667115-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1588360667115
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',

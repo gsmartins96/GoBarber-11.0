4e0e387cde73400467a8cb0f51a9a97d980a3cb1
Gabriel dos Santos Martins <gsmartins96@hotmail.com>
2020-05-07 13:38:24 -0300

    :construction: upload image

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '7c0fee07811faa998eec37f40ff6e769',
+    expiresIn: '1d',
+  },
+};
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,12 +2,16 @@ import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
+appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (requeset, response) => {
+appointmentsRouter.get('/', async (request, response) => {
+  console.log(request.user);
+
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
 
